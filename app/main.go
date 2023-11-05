64b5a833ffef33a9327a0fc695c1c1dac232dab8
Sergey Pavlov <self@sergeypavlov.dev>
2023-11-05 13:36:30 +0300

    Update console logging package

--- app/main.go
+++ app/main.go
@@ -4,20 +4,20 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"os"
+	"project/app/clog"
 )
 
 func main() {
 	fmt.Println("Go environment.")
 
-	handler := newHandler(os.Stdout, &slog.HandlerOptions{Level: LevelTrace})
+	handler := clog.NewHandler(&slog.HandlerOptions{Level: clog.LevelTrace})
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
 
-	slog.Log(context.Background(), LevelTrace, "trace log")
+	slog.Log(context.Background(), clog.LevelTrace, "trace log")
 	slog.Debug("debug log")
 	slog.Info("info log")
 	slog.Warn("warning log")
 	slog.Error("error log")
-	slog.Log(context.Background(), LevelFatal, "fatal error log")
+	slog.Log(context.Background(), clog.LevelFatal, "fatal error log")
 }
